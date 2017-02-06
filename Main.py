02cd6f00275ea605e71c3b0c95dd92858e4e44ce
Daniel Zatulovsky <DanielZa2u2@gmail.com>
2017-02-06 15:02:47 +0200

    Removed poor name fetching methods from the code

--- Main.py
+++ Main.py
@@ -90,53 +90,6 @@ def json_from_valve(file_string):
 
     return file_string
 
-''' Don't use this. Works Poorly'''
-def fetch_name_from_steamstore(app_id):
-    html_string = str(urlrequest.urlopen("http://store.steampowered.com/app/" + app_id).read())
-
-    name_start = html_string.find("<div class=\"apphub_AppName\">")
-
-    if name_start == -1 and html_string.find("agecheck"):
-        # Does not work
-        '''
-        params = {
-            "ageDay": "1",
-            "ageMonth": "January",
-            "ageYear": "1986",
-            "snr": "1_agecheck_agecheck__age-gate"
-        }
-
-        headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            #"Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "en-US,en;q=0.8,he;q=0.6",
-            "Content-type": "application/x-www-form-urlencoded",
-            # cookie
-            "DNT": "1",
-            "Origin": "http://store.steampowered.com",
-
-            "Referer": ("http://store.steampowered.com/agecheck/app/" + app_id + "/"),
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
-        }
-
-        request = urllib.request.Request("http://store.steampowered.com/agecheck/app/" + app_id + "/", urllib.parse.urlencode(params).encode(), headers)
-
-        html_string = str(urllib.request.urlopen(request).read())
-        name_start = html_string.find("<div class=\"apphub_AppName\">")
-        '''
-
-    if name_start == -1:
-        if WRITE_FAILED_NAME_FETCH_TO_FILE:
-            with open("Log/fetch_error.html.txt", 'w') as out:
-                out.write(html_string.replace("\\r", "\r").replace("\\t", "\t").replace("\\n", "\n"))
-        return None  # //Can't fetch the name
-
-    name_end = html_string.find("</div>", name_start + len("<div class=\"apphub_AppName\">"))
-    name = html_string[name_start + len("<div class=\"apphub_AppName\">"): name_end]
-
-    return name
-
 
 def fetch_game_data(app_id):
     req = urlrequest.Request("http://store.steampowered.com/api/appdetails/?appids=" + app_id)
@@ -160,28 +113,7 @@ def fetch_game_data(app_id):
 
 
 
-''' Don't use this. SteamDB asked not to. https://steamdb.info/faq/#can-i-use-auto-refreshing-plugins-or-automatically-scrape-crawl-steamdb'''
-def fetch_name_from_steamdb(app_id):
 
-    prefix = "<td itemprop=\"name\">"
-    sufix = "</td>"
-
-    req = urlrequest.Request("http://steamdb.info/app/" + app_id, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'})
-
-    html_string = str(urlrequest.urlopen(req).read())
-
-    name_start = html_string.find(prefix)
-
-    if name_start == -1:
-        if WRITE_FAILED_NAME_FETCH_TO_FILE:
-            with open("Log/fetch_error.html.txt", 'w') as out:
-                out.write(html_string.replace("\\r", "\r").replace("\\t", "\t").replace("\\n", "\n"))
-        return None  # //Can't fetch the name
-
-    name_end = html_string.find(sufix, name_start + len(prefix))
-    name = html_string[name_start + len(prefix): name_end]
-
-    return html.unescape(name)
 
 
 # main()
