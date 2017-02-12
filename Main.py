0ab67e86ede503824fe3d88a9e73046e0ef13f45
Daniel Zatulovsky <DanielZa2u2@gmail.com>
2017-02-12 15:51:55 +0200

    More code cleanup. Docstrings. renaming of methods. Slight changes to the fetch applist method making the oricess more logical. Begining of the locate steam function.

--- Main.py
+++ Main.py
@@ -18,7 +18,9 @@ OUTPUT__SHAREDCONFIG_VDF_JSON = False
 LOG_ERROR___FETCH_APPLIST = True
 
 
-class Tag:
+class Category:
+    """Describe a single steam category."""
+
     def __init__(self, name):
         self.name = name
         self.games = []
@@ -29,7 +31,8 @@ class Tag:
     def __str__(self):
         return "Tag: " + self.name
 
-    def game_string(self):
+    def app_string(self):
+        """Return human-redable string formated to contain the category and all the apps that belong to it."""
         ret = str(self)
         for game in self.games:
             ret += "\n\t"
@@ -37,18 +40,22 @@ class Tag:
         return ret
 
 
-class Tags:
+class Categories:
+    """Describe a group of categories. Mainly the result of processing the user library."""
+
     def __init__(self):
         self.lst = []
 
-    def name_games(self, applist):
+    def name_apps(self, applist):
+        """Gives a name for all the apps in all the tags based on the names stored in the applist"""
         gamelist = [game for tag in self.lst for game in tag.games]
-        applist.name_games(gamelist)
+        applist.name_apps(gamelist)
 
-    def games_string(self, filter_symbols=False):
+    def apps_string(self, filter_symbols=False):
+        """Return human-redable string formated to contain all the categories and all the apps that belong to them. Can filter © and ™ symbols from the names."""
         ret = ""
         for tag in self.lst:
-            ret += tag.game_string()
+            ret += tag.app_string()
             ret += "\n"
         if filter_symbols:
             f = lambda string: string != "©" and string != "™"
@@ -56,18 +63,40 @@ class Tags:
         else:
             return ret
 
-    def save_games_string(self, filename=None, filter_symbols=False):
+    def save_apps_string(self, filename=None, filter_symbols=False):
+        """Save the human-redable string returned by apps_string to the disk. Can filter © and ™ symbols from the names."""
         if filename is None:
-            filename = "Steam Categories " + str(datetime.datetime.now().strftime("%Y-%m-%d;%H_%M")) + ".txt"
+            if not os.path.exists("Output"):
+                os.mkdir("Output")
+            filename = "Output/Steam Categories " + str(datetime.datetime.now().strftime("%Y-%m-%d;%H_%M")) + ".txt"
 
         with open(filename, "w", encoding='UTF-8') as file:
-            file.write(self.games_string(filter_symbols))
+            file.write(self.apps_string(filter_symbols))
 
     @staticmethod
-    def apps_from_file(path):
+    def factory(path):
+        """Read all the apps the user has from sharedconfig.vdf. Return fitting Categories obj"""
+        apps = Categories.__apps_from_file__(path)
+        apps = {appID: apps[appID] for appID in apps if ("tags" in apps[appID]) and isinstance(apps[appID]["tags"], dict)}  # remove all the games without categories
+        tag_names = sorted(set([tag_name for app in apps.values() for tag_name in app["tags"].values()]))  # get all the possible tags from the different apps
+
+        ret = Categories()
+        ret.lst = [Category(tag_name) for tag_name in tag_names]
+        games = [SteamApp(game_id) for game_id in sorted(apps.keys())]
+
+        for tag in ret.lst:
+            for game in games:
+                if tag.name in apps[game.id]["tags"].values():
+                    tag.games.append(game)
+
+        return ret
+
+    @staticmethod
+    def __apps_from_file__(path):
+        """Read all the apps the user has from sharedconfig.vdf. Returns a dict. Intermidiate function."""
         with open(path, encoding='UTF-8') as file:
             file_string = file.read()
-            file_string = Tags.json_from_valve(file_string)
+            file_string = Categories.json_from_valve(file_string)
             if OUTPUT__SHAREDCONFIG_VDF_JSON:
                 with open(path + ".json", "w", encoding='UTF-8') as out:
                     out.write(file_string)
@@ -77,6 +106,7 @@ class Tags:
 
     @staticmethod
     def json_from_valve(file_string):
+        """Convert the json-like format valve uses to proper json. Used to read sharedconfig.vdf."""
         file_string = "{\n" + file_string + "}"
         # "txt" -> "txt":
         file_string = re.sub(r"(\n[^\n\"]*\"[^\n\"]*\")\n", r"\1:\n", file_string)
@@ -89,29 +119,10 @@ class Tags:
 
         return file_string
 
-    @staticmethod
-    def obj_list_from_path(path):
-        apps = Tags.apps_from_file(path)
-        apps = {appID: apps[appID] for appID in apps if ("tags" in apps[appID]) and isinstance(apps[appID]["tags"], dict)}  # remove all the games without categories
 
-        return Tags.obj_list_from_str(apps)
+class SteamApp:
+    """Describe a single steam app. That is, more often than not, a game."""
 
-    @staticmethod
-    def obj_list_from_str(apps):
-        tag_names = sorted(set([tag_name for app in apps.values() for tag_name in app["tags"].values()]))  # get all the possible tags from the different apps
-
-        tags = Tags()
-        tags.lst = [Tag(tag_name) for tag_name in tag_names]
-        games = [Game(game_id) for game_id in apps]
-
-        for tag in tags.lst:
-            for game in games:
-                if tag.name in apps[game.id]["tags"].values():
-                    tag.games.append(game)
-        return tags
-
-
-class Game:
     def __init__(self, index):
         self.id = index
         self.name = None
@@ -124,26 +135,29 @@ class Game:
 
     def __repr__(self):
         if self.name is None:
-            return "<Game: AppID_" + self.id + ">"
+            return "<SteamApp: AppID_" + self.id + ">"
         else:
-            return "<Game: " + self.name + ">"
+            return "<SteamApp: " + self.name + ">"
 
     def get_name(self, applist):
+        """Lookup your own name in the supplied list of names."""
         if self.id.isdigit():
             self.name = applist.id_lookup.get(int(self.id), None)
 
 
-class AppList:
-    def __init__(self, filename="Applist.txt", remove_trademark_symbols=True):
-        self.filename = filename
-        self.remove_trademark_symbols = remove_trademark_symbols
-        self.data = None
+class SteamAppList:
+    """Describe a list of appIDs and app names. Used to find the name of the app based on the id."""
+    FETCH_URL = "http://api.steampowered.com/ISteamApps/GetAppList/v0001/"
+    FETCH_LOCAL_PATH = "Applist.txt"
+
+    def __init__(self):
+        self.__data__ = None
         self.id_lookup = None
         # self.name_lookup = None
 
     @staticmethod
-    def fetch_from_net():
-        req = urlrequest.Request("http://api.steampowered.com/ISteamApps/GetAppList/v0001/")
+    def fetch_from_net(url=FETCH_URL):
+        req = urlrequest.Request(url)
         try:
             json_bytes = urlrequest.urlopen(req).read()
         except urlerror.HTTPError as e:
@@ -153,55 +167,52 @@ class AppList:
 
         return json_bytes.decode("utf-8")
 
-    def fetch_from_disk(self):
-        with open(self.filename, encoding='UTF-8') as file:
+    @staticmethod
+    def fetch_from_disk(path=FETCH_LOCAL_PATH):
+        with open(path, encoding='UTF-8') as file:
             return file.read()
 
     @staticmethod
-    def json_to_obj(json_text):
+    def write_apps_to_disk(data, path=FETCH_LOCAL_PATH):
+        with open(path, "w", encoding='UTF-8') as file:
+            file.write(data)
+
+    @staticmethod
+    def json_to_list(json_text):
+        """Parse the json data and turn it into a list of id-name pairs"""
         try:
             game_info = json.loads(json_text)
-
-            data = game_info["applist"]["apps"]["app"]
-            return data
-
+            return game_info["applist"]["apps"]["app"]
 
         except (json.decoder.JSONDecodeError, KeyError) as e:
             if LOG_ERROR___FETCH_APPLIST:
                 log(str(e) + "\n\n\n" + json_text, "ParseError_")
             return None
 
-    def write_apps_to_disk(self, data):
-        with open(self.filename, "w", encoding='UTF-8') as file:
-            file.write(data)
-
-    def name_games(self, gamelist):
-        for game in gamelist:
+    def name_apps(self, lst):
+        """Give names to all the apps in the list,"""
+        for game in lst:
             game.get_name(self)
 
     def fetch(self, fetch_from_net=False):
-        if self.data is not None:
+        """Fill the object with data about app names. get the data either from a local file or from the internet. Automaticlly access the net if the file is missing."""
+        if self.__data__ is not None:
             return self
 
-        if not fetch_from_net and not os.path.exists(self.filename):
-            print("Can't locate file")
-            return None
+        if fetch_from_net or not os.path.exists(SteamAppList.FETCH_LOCAL_PATH):
+            json_text = SteamAppList.fetch_from_net()
+            self.__data__ = SteamAppList.json_to_list(json_text)
+            SteamAppList.write_apps_to_disk(json_text)
+        else:
+            self.__data__ = SteamAppList.json_to_list(SteamAppList.fetch_from_disk())
 
-        if not fetch_from_net:  # and os.path.exists(self.filename)
-            self.data = AppList.json_to_obj(self.fetch_from_disk())
-
-        if fetch_from_net or not os.path.exists(self.filename):
-            json_text = AppList.fetch_from_net()
-            self.data = AppList.json_to_obj(json_text)
-            self.write_apps_to_disk(json_text)
-
-        self.id_lookup = {pair["appid"]: pair["name"] for pair in self.data}
+        self.id_lookup = {pair["appid"]: pair["name"] for pair in self.__data__}
         # self.name_lookup = {pair["name"]: pair["appid"] for pair in self.data}
-
         return self
 
 
 def log(msg, prefix="Error_", filename=None):
+    """Logs and error as sperate file inside the ./Log dir"""
     if not os.path.exists("Log"):
         os.mkdir("Log")
 
@@ -212,15 +223,30 @@ def log(msg, prefix="Error_", filename=None):
         file.write(msg)
 
 
+def locate_steam():
+    """Find the installation directory on steam and return all the possible locations of sharedconfig.vdf"""
+    return "Test\\sharedconfig.vdf.txt"
+
+    results = []
+    if os.path.exists("C:/Program Files (x86)/Steam/Steam.exe") and os.path.exists("C:/Program Files (x86)/Steam/userdata/"):
+        pass
+
+    # check C/program files
+    # search for steam.exe -> sharedconfig.vdf
+    # magic?
+
+    return results
+
+
 def main():
-    default_path = "Test\\sharedconfig.vdf.txt"
+    default_path = locate_steam()
 
-    applist = AppList().fetch()
-    tags = Tags.obj_list_from_path(default_path)
-    tags.name_games(applist)
+    applist = SteamAppList().fetch()
+    tags = Categories.factory(default_path)
+    tags.name_apps(applist)
 
-    print(tags.games_string(True))
-    tags.save_games_string(filter_symbols=True)
+    print(tags.apps_string(True))
+    tags.save_apps_string(filter_symbols=True)
 
 
 main()
